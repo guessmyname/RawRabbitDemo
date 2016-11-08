1898cfbcc0c0633be069b3c8f2752f42392e7f20
Brad Tillman <guessmyname@gmail.com>
2016-11-07 21:56:41 -0500

    RR Demo

---
+++ RawRabbitDemo/RabbitDemo/SubscribeRawRabbit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RawRabbit.Configuration.Exchange;
+using RawRabbit.vNext;
+
+namespace RabbitDemo
+{
+   public class SubscribeRawRabbit
+    {
+
+        public void subscribe()
+        {
+            var client = BusClientFactory.CreateDefault();
+            client.SubscribeAsync<string>(async (msg, context) =>
+            {
+                await Console.Out.WriteAsync($"Recieved: {msg}.");
+            }, config =>
+            {
+                config.WithExchange(exchange => exchange.WithName("logs")
+                .WithType(ExchangeType.Fanout));
+            });
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
